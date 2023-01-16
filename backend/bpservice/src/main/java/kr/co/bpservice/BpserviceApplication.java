ab11ee2c7a2b71ff0e9c08071c37d3aa1a725465
rigizer <rigizer@gmail.com>
2023-01-16 10:10:45 +0900

    [BE] ADD : #S08P12D104-63 backend project init

---
+++ backend/bpservice/src/main/java/kr/co/bpservice/BpserviceApplication.java
@@ -0,0 +1,13 @@
+package kr.co.bpservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BpserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BpserviceApplication.class, args);
+	}
+
+}
