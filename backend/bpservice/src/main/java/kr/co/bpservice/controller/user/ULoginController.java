5b1fc6473b7814e5072903d622ccb02329574204
rhalsemd <rhalsemd@naver.com>
2023-01-18 10:04:38 +0900

    [BE] UPDATE : #S08P12D104-99 SpringSecurity 적용

--- backend/bpservice/src/main/java/kr/co/bpservice/controller/user/ULoginController.java
+++ backend/bpservice/src/main/java/kr/co/bpservice/controller/user/ULoginController.java
@@ -1,9 +1,22 @@
 package kr.co.bpservice.controller.user;
 
 import kr.co.bpservice.service.user.ULoginService;
+import kr.co.bpservice.util.auth.dto.MemberRequestDto;
+import kr.co.bpservice.util.auth.dto.TokenDto;
+import kr.co.bpservice.util.auth.service.AuthService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class ULoginController {
+    private final AuthService authService;
+    @PostMapping("/login")
+    public ResponseEntity<TokenDto> login(@RequestBody MemberRequestDto requestDto) {
+        return ResponseEntity.ok(authService.login(requestDto));
+    }
 }
--- backend/bpservice/src/main/java/kr/co/bpservice/controller/user/USignUpController.java
+++ backend/bpservice/src/main/java/kr/co/bpservice/controller/user/USignUpController.java
@@ -1,7 +1,20 @@
 package kr.co.bpservice.controller.user;
 
+import kr.co.bpservice.util.auth.dto.MemberRequestDto;
+import kr.co.bpservice.util.auth.dto.MemberResponseDto;
+import kr.co.bpservice.util.auth.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class USignUpController {
+    private final AuthService authService;
+    @PostMapping("/signup")
+    public ResponseEntity<MemberResponseDto> signup(@RequestBody MemberRequestDto requestDto) {
+        return ResponseEntity.ok(authService.signup(requestDto));
+    }
 }
