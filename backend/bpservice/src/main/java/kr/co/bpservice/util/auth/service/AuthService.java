c8baa43bb7757a3a73c42a1371e9644b2c34f715
miracle3070 <miracle3070@naver.com>
2023-01-19 12:45:20 +0900

    [BE] UPDATE : #S08P12D104-142 회원가입 아이디, 비밀번호 검증 추가
    Former-commit-id: 11b3846108a128061bfe84b5b08bb8cf0b86619f

--- backend/bpservice/src/main/java/kr/co/bpservice/util/auth/service/AuthService.java
+++ backend/bpservice/src/main/java/kr/co/bpservice/util/auth/service/AuthService.java
@@ -16,6 +16,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.regex.Pattern;
 
 @Service
 @RequiredArgsConstructor
@@ -27,8 +28,28 @@ public class AuthService {
     private final TokenProvider tokenProvider;
 
     public UserResponseDto join(UserRequestDto requestDto) {
-        if (userRepository.existsById(requestDto.getUserId())) {
+        String userId = requestDto.getUserId();
+        String pwd = requestDto.getPwd();
+        String strPattern = "^[a-z0-9]*$";
+
+        if(userId.length() < 8 || userId.length() > 20) {
+            throw new RuntimeException("아이디는 8~20자로 설정해야합니다.");
+        }
+
+        if(Pattern.matches(strPattern, userId) == false) {
+            throw new RuntimeException("아이디는 영어소문자, 숫자만 가능합니다.");
+        }
+
+        if (userRepository.existsById(userId)) {
             throw new RuntimeException("이미 가입되어 있는 유저입니다");
+        }
+
+        if(pwd.length() < 8 || pwd.length() > 20) {
+            throw new RuntimeException("비밀번호는 8~20자로 설정해야합니다.");
+        }
+
+        if(Pattern.matches(strPattern, pwd) == false) {
+            throw new RuntimeException("비밀번호는 영어소문자, 숫자만 가능합니다.");
         }
 
         User user = requestDto.toUser(passwordEncoder);
