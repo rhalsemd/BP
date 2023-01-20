a6403f1084c7d27461904926b8972185d387cd83
rigizer <rigizer@gmail.com>
2023-01-20 14:54:49 +0900

    [BE] ADD : HTTP 응답코드를 처리하는 HTTPUtils 추가
    Former-commit-id: 7002e3dee75d9fed7f3c3bc7b99a1432c12b892e

---
+++ backend/bpservice/src/main/java/kr/co/bpservice/util/HTTPUtils.java
@@ -0,0 +1,16 @@
+package kr.co.bpservice.util;
+
+public class HTTPUtils {
+    public static int HTTP_OK = 200;
+
+    public static int HTTP_BAD_REQUEST = 401;
+    public static int HTTP_FORBIDDEN = 403;
+    public static int HTTP_PAGE_NOT_FOUND = 404;
+    public static int HTTP_METHOD_NOT_ALLOWED = 405;
+
+    public static int HTTP_INTERNAL_SERVER_ERROR = 500;
+    public static int HTTP_BAD_GATEWAY = 502;
+    public static int HTTP_SERVICE_UNAVAILABLE = 503;
+    public static int HTTP_GATEWAY_TIMEOUT = 504;
+
+}
