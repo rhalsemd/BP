592e41c33b99747dcb608dbfab5aee3564fdffb7
rigizer <rigizer@gmail.com>
2023-01-22 22:21:04 +0900

    [BE] ADD : Header 객체에 대한 메소드 추가 (append, getValue, getKeyList, copy)
    Former-commit-id: 2c489eb4b382da1bbabd46892f89513ca3758670

--- backend/bpservice/src/main/java/kr/co/bpservice/util/network/Header.java
+++ backend/bpservice/src/main/java/kr/co/bpservice/util/network/Header.java
@@ -1,5 +1,32 @@
 package kr.co.bpservice.util.network;
 
-public class Header {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+public class Header {
+    Map<String, String> headers = new HashMap<>();
+
+    public void append(String key, String value) {
+        headers.put(key, value);
+    }
+
+    public String getValue(String key) {
+        return headers.get(key);
+    }
+
+    public List<String> getKeyList() {
+        List<String> keyList = new ArrayList<>();
+        headers.keySet().forEach(key -> keyList.add(key));
+
+        return keyList;
+    }
+
+    public Header copy() {
+        Header header = new Header();
+        headers.keySet().forEach(key -> header.append(key, headers.get(key)));
+
+        return header;
+    }
 }
