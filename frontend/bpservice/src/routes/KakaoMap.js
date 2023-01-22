45ae9a94499832f5bf5aa37923d5bec55fc0391c
ggnsall <ggnsall@naver.com>
2023-01-22 11:36:38 +0900

    [FE] UPDATE : react-kakao-maps-sdk로 작성한 컴포넌트로 교체
    Former-commit-id: 45c6a7b2271d9eba96974659c2977274dc1bffbb

--- frontend/bpservice/src/routes/KakaoMap.js
+++ frontend/bpservice/src/routes/KakaoMap.js
@@ -1,38 +1,77 @@
-/** @jsxImportSource @emotion/react */
-import { css } from "@emotion/react";
 import { Suspense, lazy } from "react";
 
-// import Map from "../components/kakaoMap/Map";
-const Map = lazy(() => import("../components/kakaoMap/Map"));
-// import MapSearchResult from "../components/kakaoMap/MapSearchResult";
-
-const outertopContainer = css`
-  box-sizing: border-box;
-`;
-
-const topContainer = css`
-  position: sticky;
-  top: 0px;
-`;
-
-// const bottomContainer = css`
-//   position: static;
-// `;
+import { useState } from "react";
+import { Map } from "react-kakao-maps-sdk";
+import styled from "../style/Map.module.css";
+// import EventMarkerContainer from "../components/kakaoMap/EventMarkerContainer";
+const EventMarkerContainer = lazy(() =>
+  import("../components/kakaoMap/EventMarkerContainer")
+);
 
 function KakaoMap() {
+  const positions = [
+    {
+      title: "카카오",
+      latlng: { lat: 33.450705, lng: 126.570677 },
+      isOpen: false,
+    },
+    {
+      title: "생태연못",
+      latlng: { lat: 33.450936, lng: 126.569477 },
+      isOpen: false,
+    },
+    {
+      title: "텃밭",
+      latlng: { lat: 33.450879, lng: 126.56994 },
+      isOpen: false,
+    },
+    {
+      title: "근린공원",
+      latlng: { lat: 33.451393, lng: 126.570738 },
+      isOpen: false,
+    },
+  ];
+  const [location, setLocation] = useState({ lat: 33.450705, lng: 126.570677 });
+
+  const currentBtn = () => {
+    console.log("wqrew");
+  };
+
   return (
-    <Suspense fallback={<p>Loding</p>}>
-      <div css={outertopContainer}>
-        <div css={topContainer}>
+    <>
+      <Suspense
+        fallback={
           <div>
-            <Map />
+            <h1>Loding</h1>
           </div>
-        </div>
-        {/* <footer css={bottomContainer}>
-        <MapSearchResult />
-      </footer> */}
-      </div>
-    </Suspense>
+        }
+      >
+        <Map // 지도를 표시할 Container
+          id={`map`}
+          // 지도의 중심좌표
+          center={location}
+          style={{
+            // 지도의 크기
+            width: "100%",
+            height: "100vh",
+          }}
+          level={1} // 지도의 확대 레벨
+        >
+          {positions.map((position, index) => (
+            <div key={`${position.title}-${position.latlng}`}>
+              <EventMarkerContainer
+                position={position}
+                index={index}
+                positions={positions}
+              />
+            </div>
+          ))}
+          <button className={styled.currentBtn} onClick={currentBtn}>
+            +
+          </button>
+        </Map>
+      </Suspense>
+    </>
   );
 }
 
