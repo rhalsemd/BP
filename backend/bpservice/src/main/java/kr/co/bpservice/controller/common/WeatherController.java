e6bbbb960c4af4fd27f9587b795e9819b26f0325
rigizer <rigizer@gmail.com>
2023-01-25 10:48:39 +0900

    [BE] UPDATE : currentWeather의 위도, 경도, 현재 기온, 최저온도, 최고온도, 체감온도, 날씨, 바람세기, 강수량, 이미지 데이터 가져오기
    Former-commit-id: 2d7829e70ad8f2537665663eace706267bb13472

--- backend/bpservice/src/main/java/kr/co/bpservice/controller/common/WeatherController.java
+++ backend/bpservice/src/main/java/kr/co/bpservice/controller/common/WeatherController.java
@@ -4,15 +4,11 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.Parameters;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import kr.co.bpservice.util.HTTPUtils;
-import kr.co.bpservice.util.JSONUtils;
+import kr.co.bpservice.service.common.WeatherService;
 import lombok.RequiredArgsConstructor;
 import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.URL;
 
 @Tag(name = "Weather", description = "날씨 정보 API")
 @RequestMapping("/api/weather")
@@ -20,25 +16,17 @@ import java.net.URL;
 @RestController
 @CrossOrigin("*")
 public class WeatherController {
-    private final String APP_ID = "d99a9cbb680b4e229b71185d0c2f7e0c";
+    @Autowired
+    WeatherService weatherService;
+
     @GetMapping("/current-weather")
     @Operation(description = "위도, 경도와 일치하는 지역에 대한 현재 날씨정보 조회")
     @Parameters({@Parameter(name = "lat", description = "날씨를 조회할 지역의 위도")
                 ,@Parameter(name = "lng", description = "날씨를 조회할 지역의 경도")
     })
-    public JSONObject currentWeather(@RequestParam float lat, @RequestParam float lng) throws IOException {
-        URL currentWeatherUrl = new URL(String.format("https://api.openweathermap.org/data/2.5/weather?lang=kr&units=metric&lat=%s&lon=%s&appid=%s", String.valueOf(lat), String.valueOf(lng), APP_ID));
-        HttpURLConnection conn = (HttpURLConnection) currentWeatherUrl.openConnection();
-
-        JSONObject jsonObject = new JSONObject();
-
-        int responseCode = conn.getResponseCode();
-        if (responseCode != HttpURLConnection.HTTP_OK) {
-            jsonObject.put("response-code", responseCode);
-            return jsonObject;
-        }
-
-        jsonObject = new JSONObject();
-        return jsonObject;
+    public String currentWeather(@RequestParam double lat, @RequestParam float lng) {
+        JSONObject temp = weatherService.currentWeather(lat, lng);
+        System.out.println(temp);
+        return temp.toString();
     }
 }
