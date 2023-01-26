ad87371aa2edd8efed9dfe17e2e8f6ff4c69c8ec
rigizer <rigizer@gmail.com>
2023-01-26 09:49:49 +0900

    [BE] UPDATE : 키오스크 지점 목록을 제공하는 API 구현
    Former-commit-id: f6f61806a5c8c40ee6b1d10053d0f3b94b97318e

--- backend/bpservice/src/main/java/kr/co/bpservice/controller/kiosk/KHomeController.java
+++ backend/bpservice/src/main/java/kr/co/bpservice/controller/kiosk/KHomeController.java
@@ -4,12 +4,18 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.Parameters;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import kr.co.bpservice.entity.brolly.Brolly;
+import kr.co.bpservice.entity.brolly.BrollyCase;
 import kr.co.bpservice.service.kiosk.KHomeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@Tag(name = "Kiosk Home", description = "키오스크 홈에서 사용하는 API")
+import java.util.List;
+
+@Tag(name = "Kiosk Home", description = "키오스크 지점 관련 정보를 제공하기 위한 API")
 @RequestMapping("/api/kiosk/home")
 @RequiredArgsConstructor
 @RestController
@@ -19,11 +25,19 @@ public class KHomeController {
     KHomeService kHomeService;
 
     @GetMapping("/kiosk-geo")
-    @Operation(description = "키오스크 지점 고유번호에 해당하는 위도, 경도 데이터 조회")
+    @Operation(description = "키오스크 지점 고유번호에 해당하는 위도, 경도, 지점명 데이터 조회")
     @Parameters({
             @Parameter(name = "id", description = "키오스크 지점 고유번호")
     })
-    public String getKioskGeo(@RequestParam int id) {
-        return kHomeService.getKioskGeo(id);
+    public ResponseEntity<BrollyCase> getKioskGeo(@RequestParam int id) {
+        BrollyCase brollyCase = kHomeService.getKioskGeo(id);
+        return new ResponseEntity<>(brollyCase, HttpStatus.OK);
+    }
+
+    @GetMapping("/kiosk-list")
+    @Operation(description = "키오스크 지점 조회")
+    public ResponseEntity<List<BrollyCase>> getBrollyCaseList() {
+        List<BrollyCase> brollyCaseList = kHomeService.getBrollyCaseList();
+        return new ResponseEntity<>(brollyCaseList, HttpStatus.OK);
     }
 }
