031fbd9c0452c9fd974b73a5ed6778932941e59e
rhalsemd <rhalsemd@naver.com>
2023-01-26 17:39:27 +0900

    [BE] UPDATE : 관리자 통계 URL 변경
    Former-commit-id: b6d4b4db7a24ebbfbe4ac5d53729ed7a644c4a36

--- backend/bpservice/src/main/java/kr/co/bpservice/controller/admin/ASysManageController.java
+++ backend/bpservice/src/main/java/kr/co/bpservice/controller/admin/ASysManageController.java
@@ -11,44 +11,45 @@ import java.util.Map;
 
 @RestController
 @CrossOrigin("*")
+@RequestMapping("/api/auth/admin")
 public class ASysManageController {
     @Autowired
     private ASysManageService aSysManageService;
     //전체 지점별 일 수익
-    @GetMapping("/admin/AllKioskMoneyDay/{day}")
+    @GetMapping("/AllKioskMoneyDay/{day}")
     public List<Map<String, String>> AllKioskMoneyDay(@PathVariable("day") String day){
         List<Map<String, String>> returnDay = aSysManageService.SAllKioskMoneyDay(day);
         return returnDay;
     }
     //전체 지점별 월 수익
-    @GetMapping("/admin/AllKioskMoneyMonth/{month}/{year}")
+    @GetMapping("/AllKioskMoneyMonth/{month}/{year}")
     public List<Map<String, String>> AllKioskMoneyMonth(@PathVariable("month") String month, @PathVariable("year") String year){
         String callMonth=year+"-"+month;
         List<Map<String, String>> returnMonth = aSysManageService.SAllKioskMoneyMonth(callMonth);
         return returnMonth;
     }
     //전체 지점별 일 이용내역
-    @GetMapping("/admin/AllKioskCountDay/{day}")
+    @GetMapping("/AllKioskCountDay/{day}")
     public List<Map<String, String>> AllKioskCountDay(@PathVariable("day") String day){
         List<Map<String, String>> returnDay = aSysManageService.SAllKioskCountDay(day);
         return returnDay;
     }
     //전체 지점별 월 이용내역
-    @GetMapping("/admin/AllKioskCountMonth/{month}/{year}")
+    @GetMapping("/AllKioskCountMonth/{month}/{year}")
     public List<Map<String, String>> AllKioskCountMonth(@PathVariable("month") String month, @PathVariable("year") String year){
         String callMonth=year+"-"+month;
         List<Map<String, String>> returnMonth = aSysManageService.SAllKioskCountMonth(callMonth);
         return returnMonth;
     }
     //해당 지점 일 매출
-    @GetMapping("/admin/KioskMoneyMonth/{month}/{year}/{id}")
+    @GetMapping("/KioskMoneyMonth/{month}/{year}/{id}")
     public List<Map<String, String>> KioskMoneyMonth(@PathVariable("month") String month, @PathVariable("year") String year,@PathVariable("id") String id){
         String callMonth=year+"-"+month;
         List<Map<String, String>> returnDay = aSysManageService.SKioskMoneyMonth(callMonth,id);
         return returnDay;
     }
     //해당 지점 월 매출
-    @GetMapping("/admin/KioskMoneyYear/{year}/{id}")
+    @GetMapping("/KioskMoneyYear/{year}/{id}")
     public List<Map<String, String>> KioskMoneyYear(@PathVariable("year") String year,@PathVariable("id") String id){
         List<Map<String, String>> returnMonth = aSysManageService.SKioskMoneyYear(year,id);
         return returnMonth;
