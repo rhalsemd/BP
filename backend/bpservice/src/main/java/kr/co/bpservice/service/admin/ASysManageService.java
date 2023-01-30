de54c215d385bebff0a9a568e0ee2aa13756ba12
rhalsemd <rhalsemd@naver.com>
2023-01-30 16:30:23 +0900

    [BE] UPDATE : 통계 관련 서비스, 컨트롤러,repository api 및 메소드 이름 변경
    Former-commit-id: 88b47f72ce46cc66a1073cf008356f23556e10c8

--- backend/bpservice/src/main/java/kr/co/bpservice/service/admin/ASysManageService.java
+++ backend/bpservice/src/main/java/kr/co/bpservice/service/admin/ASysManageService.java
@@ -14,29 +14,29 @@ public class ASysManageService {
     @Autowired
     private ASysManageRepository aSysManageRepository;
 
-    public List<Map<String, String>> SAllKioskMoneyDay(String paramDay){
-        List<Map<String, String>> returnday = aSysManageRepository.RALLKioskMoneyDay(paramDay, "money");
+    public List<Map<String, String>> sAllKioskMoneyDay(String paramDay){
+        List<Map<String, String>> returnday = aSysManageRepository.rALLKioskMoneyDay(paramDay, "money");
         return returnday;
     }
-    public List<Map<String, String>> SAllKioskMoneyMonth(String paramMONTH){
-        List<Map<String, String>> returnmonth = aSysManageRepository.RALLKioskMoneyMonth(paramMONTH,"money");
+    public List<Map<String, String>> sAllKioskMoneyMonth(String paramMONTH){
+        List<Map<String, String>> returnmonth = aSysManageRepository.rALLKioskMoneyMonth(paramMONTH,"money");
         return returnmonth;
     }
 
-    public List<Map<String, String>> SAllKioskCountDay(String paramDay){
-        List<Map<String, String>> returnday = aSysManageRepository.RALLKioskMoneyDay(paramDay, "count");
+    public List<Map<String, String>> sAllKioskCountDay(String paramDay){
+        List<Map<String, String>> returnday = aSysManageRepository.rALLKioskMoneyDay(paramDay, "count");
         return returnday;
     }
-    public List<Map<String, String>> SAllKioskCountMonth(String paramMONTH){
-        List<Map<String, String>> returnmonth = aSysManageRepository.RALLKioskMoneyMonth(paramMONTH,"count");
+    public List<Map<String, String>> sAllKioskCountMonth(String paramMONTH){
+        List<Map<String, String>> returnmonth = aSysManageRepository.rALLKioskMoneyMonth(paramMONTH,"count");
         return returnmonth;
     }
-    public List<Map<String, String>> SKioskMoneyMonth(String paramMonth, String paramid){
-        List<Map<String, String>> returnmonth = aSysManageRepository.RKioskMoneyYearMonth(paramMonth, paramid,"month");
+    public List<Map<String, String>> sKioskMoneyMonth(String paramMonth, String paramid){
+        List<Map<String, String>> returnmonth = aSysManageRepository.rKioskMoneyYearMonth(paramMonth, paramid,"month");
         return returnmonth;
     }
-    public List<Map<String, String>> SKioskMoneyYear(String paramYear, String paramid){
-        List<Map<String, String>> returnyear = aSysManageRepository.RKioskMoneyYearMonth(paramYear,paramid,"year");
+    public List<Map<String, String>> sKioskMoneyYear(String paramYear, String paramid){
+        List<Map<String, String>> returnyear = aSysManageRepository.rKioskMoneyYearMonth(paramYear,paramid,"year");
         return returnyear;
     }
 }
