ea56e4cfc6dead17fb1cc6d95a7db0befdaa026e
rhalsemd <rhalsemd@naver.com>
2023-01-30 22:36:04 +0900

    [BE]UPDATE : 백엔드 admin 로그 정보 전달 기능 완료
    Former-commit-id: f5c1e73bb4c7df20b01ced1a2fbe5d544047bb60

--- backend/bpservice/src/main/java/kr/co/bpservice/controller/admin/ALogController.java
+++ backend/bpservice/src/main/java/kr/co/bpservice/controller/admin/ALogController.java
@@ -1,6 +1,6 @@
 package kr.co.bpservice.controller.admin;
 
-import kr.co.bpservice.entity.user.User;
+import kr.co.bpservice.entity.user.UserExceptPass;
 import kr.co.bpservice.service.admin.ALogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -16,8 +16,20 @@ public class ALogController {
     private ALogService aLogService;
 
     @GetMapping("/get-user-data")
-    public List<User> getUserData(){
-        List<User> returnDay = null;
+    public List<UserExceptPass> getUserData(){
+        List<UserExceptPass> returnDay = aLogService.getUserData();
         return returnDay;
     }
+
+    @GetMapping("/get-lent-log-data/{userId}")
+    public List<Map<String,?>> getLentLogData(@PathVariable("userId") String userId){
+        List<Map<String,?>> returnDay = aLogService.getRentLogData(userId);
+        return returnDay;
+    }
+
+    @GetMapping("/get-img-url/{id}")
+    public String getImgUrl(@PathVariable("id") int id){
+        return aLogService.getImgUrl(id);
+    }
+
 }
