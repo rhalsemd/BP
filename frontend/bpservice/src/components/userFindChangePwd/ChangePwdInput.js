5a19035ae0b8841a5f029afdf605fd6214e888c6
ggnsall <ggnsall@naver.com>
2023-02-01 15:45:13 +0900

    [FE] UPDATE : 비밀번호 찾기 컴포넌트 CSS
    Former-commit-id: 8a9f6b4ee1d8fb5c41dc2296da1297aa3cb6d4a9

--- frontend/bpservice/src/components/userFindChangePwd/ChangePwdInput.js
+++ frontend/bpservice/src/components/userFindChangePwd/ChangePwdInput.js
@@ -1,3 +1,20 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+
+const inputBox = css`
+  position: relative;
+  margin: 10px 0;
+`;
+
+const inputChild = css`
+  background: transparent;
+  border: none;
+  border-bottom: solid 1px #ccc;
+  padding: 20px 0px 5px 0px;
+  font-size: 14pt;
+  width: 100%;
+`;
+
 function ChangePwdInput({ setInfo, pwdRegExp, info }) {
   const onChange = (e) => {
     const inputValue = e.target.value;
@@ -7,17 +24,19 @@ function ChangePwdInput({ setInfo, pwdRegExp, info }) {
   };
 
   return (
-    <div>
+    <div css={inputBox}>
       <form>
-        <label htmlFor="currentPwd">변경 할 비밀번호 : </label>
         <input
           type="password"
           id="currentPwd"
           autoComplete="off"
+          css={inputChild}
           required
           placeholder="변경 할 비밀번호"
           onChange={onChange}
         />
+        <label htmlFor="currentPwd">변경 비밀번호</label>
+
         {/* 비밀번호 조건 */}
         {pwdRegExp.test(info.pwd) || info.pwd.length === 0 ? null : (
           <div>
