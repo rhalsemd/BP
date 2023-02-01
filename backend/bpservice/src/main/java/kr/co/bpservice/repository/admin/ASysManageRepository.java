d56a9d3f7c9ed83d387daded04863a0df65a4b1b
rhalsemd <rhalsemd@naver.com>
2023-02-01 17:31:12 +0900

    [BE] UPDATE : Admin 통계 기능 없는 일 수 0으로 채우기
    Former-commit-id: a517ef2b85a930e32eaf25193394842317371005

--- backend/bpservice/src/main/java/kr/co/bpservice/repository/admin/ASysManageRepository.java
+++ backend/bpservice/src/main/java/kr/co/bpservice/repository/admin/ASysManageRepository.java
@@ -13,24 +13,52 @@ import java.util.Map;
 
 @Repository
 public interface ASysManageRepository extends JpaRepository<BrollyRentLog,Integer> {
-    @Query(value = "SELECT MBRL.CASE_ID ,SUM(MBRL.RENT_MONEY) AS TOTALMONEY, MBC.NAME\n" +
+    @Query(value = "SELECT *\n" +
+            "FROM\n" +
+            "\t(SELECT MBRL.CASE_ID ,SUM(MBRL.RENT_MONEY) AS TOTALMONEY, MBC.NAME\n" +
             "\tFROM MWS_BROLLY_RENT_LOG MBRL INNER JOIN MWS_BROLLY_CASE MBC ON MBRL.CASE_ID = MBC.ID\n" +
-            "\tWHERE MBRL.STATE = 1 AND DATE(MBRL.UPT_DT) = :paramDay GROUP BY MBRL.CASE_ID;", nativeQuery = true)
+            "\tWHERE MBRL.STATE = 1 AND DATE(MBRL.UPT_DT) = :paramDay GROUP BY MBRL.CASE_ID\n" +
+            "\tUNION\n" +
+            "\tSELECT F.ID as CASE_ID ,0 AS TOTALMONEY ,F.NAME\n" +
+            "\tFROM MWS_BROLLY_CASE F\n" +
+            "\tGROUP BY CASE_ID) TOTAL\n" +
+            "GROUP BY TOTAL.CASE_ID;", nativeQuery = true)
     List<Map<String, String>> rALLKioskMoneyDay(@Param("paramDay") String paramDay);
 
-    @Query(value = "SELECT MBRL.CASE_ID ,COUNT(MBRL.RENT_MONEY) AS TOTALCOUNT, MBC.NAME\n" +
+    @Query(value = "SELECT *\n" +
+            "FROM\n" +
+            "\t(SELECT MBRL.CASE_ID ,COUNT(MBRL.RENT_MONEY) AS TOTALCOUNT, MBC.NAME\n" +
             "\tFROM MWS_BROLLY_RENT_LOG MBRL INNER JOIN MWS_BROLLY_CASE MBC ON MBRL.CASE_ID = MBC.ID\n" +
-            "\tWHERE MBRL.STATE = 1 AND DATE(MBRL.UPT_DT) = :paramDay GROUP BY MBRL.CASE_ID;", nativeQuery = true)
+            "\tWHERE MBRL.STATE = 1 AND DATE(MBRL.UPT_DT) = :paramDay GROUP BY MBRL.CASE_ID\n" +
+            "\tUNION\n" +
+            "\tSELECT F.ID as CASE_ID ,0 AS TOTALCOUNT ,F.NAME\n" +
+            "\tFROM MWS_BROLLY_CASE F\n" +
+            "\tGROUP BY CASE_ID) TOTAL\n" +
+            "GROUP BY TOTAL.CASE_ID;", nativeQuery = true)
     List<Map<String, String>> rALLKioskCountDay(@Param("paramDay") String paramDay);
 
-    @Query(value = "SELECT MBRL.CASE_ID ,SUM(MBRL.RENT_MONEY) AS TOTALMONEY, MBC.NAME\n" +
+    @Query(value = "SELECT *\n" +
+            "FROM\n" +
+            "\t(SELECT MBRL.CASE_ID ,SUM(MBRL.RENT_MONEY) AS TOTALMONEY, MBC.NAME\n" +
             "\tFROM MWS_BROLLY_RENT_LOG MBRL INNER JOIN MWS_BROLLY_CASE MBC ON MBRL.CASE_ID = MBC.ID\n" +
-            "\tWHERE MBRL.STATE = 1 AND DATE_FORMAT(MBRL.UPT_DT,'%Y-%m') = :paramMonth GROUP BY MBRL.CASE_ID;", nativeQuery = true)
+            "\tWHERE MBRL.STATE = 1 AND DATE_FORMAT(MBRL.UPT_DT,'%Y-%m') = :paramMonth GROUP BY MBRL.CASE_ID\n" +
+            "\tUNION\n" +
+            "\tSELECT F.ID as CASE_ID ,0 AS TOTALMONEY ,F.NAME\n" +
+            "\tFROM MWS_BROLLY_CASE F\n" +
+            "\tGROUP BY CASE_ID) TOTAL\n" +
+            "GROUP BY TOTAL.CASE_ID;", nativeQuery = true)
     List<Map<String, String>> rALLKioskMoneyMonth(@Param("paramMonth") String paramMonth);
 
-    @Query(value = "SELECT MBRL.CASE_ID ,COUNT(MBRL.RENT_MONEY) AS TOTALCOUNT, MBC.NAME\n" +
+    @Query(value = "SELECT *\n" +
+            "FROM\n" +
+            "\t(SELECT MBRL.CASE_ID ,COUNT(MBRL.RENT_MONEY) AS TOTALCOUNT, MBC.NAME\n" +
             "\tFROM MWS_BROLLY_RENT_LOG MBRL INNER JOIN MWS_BROLLY_CASE MBC ON MBRL.CASE_ID = MBC.ID\n" +
-            "\tWHERE MBRL.STATE = 1 AND DATE_FORMAT(MBRL.UPT_DT,'%Y-%m') = :paramMonth GROUP BY MBRL.CASE_ID;", nativeQuery = true)
+            "\tWHERE MBRL.STATE = 1 AND DATE_FORMAT(MBRL.UPT_DT,'%Y-%m') = :paramMonth GROUP BY MBRL.CASE_ID\n" +
+            "\tUNION\n" +
+            "\tSELECT F.ID as CASE_ID ,0 AS TOTALCOUNT ,F.NAME\n" +
+            "\tFROM MWS_BROLLY_CASE F\n" +
+            "\tGROUP BY CASE_ID) TOTAL\n" +
+            "GROUP BY TOTAL.CASE_ID;", nativeQuery = true)
     List<Map<String, String>> rALLKioskCountMonth(@Param("paramMonth") String paramMonth);
 
     @Query(value = "SELECT SUM(RENT_MONEY) AS TOTALMoney ,UPT_DT as FINALDT\n" +
