99033ecc94e366b85527775dd189fc97aa38a275
ggnsall <ggnsall@naver.com>
2023-02-04 22:52:47 +0900

    [FE] UPDATE : 네비게이션바 Icon 동적으로 변경
    Former-commit-id: 579d7674f70eb0bb97a767c71c8786e49e097038

--- frontend/bpservice/src/components/Nav.js
+++ frontend/bpservice/src/components/Nav.js
@@ -1,6 +1,6 @@
 /** @jsxImportSource @emotion/react */
-
 import { css } from "@emotion/react";
+
 import * as React from "react";
 import Box from "@mui/material/Box";
 import Drawer from "@mui/material/Drawer";
@@ -11,8 +11,13 @@ import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
-import MailIcon from "@mui/icons-material/Mail";
+
+import LogoutIcon from "@mui/icons-material/Logout";
+import PermIdentityIcon from "@mui/icons-material/PermIdentity";
+import FmdGoodIcon from "@mui/icons-material/FmdGood";
+import LoginIcon from "@mui/icons-material/Login";
+import AssignmentIcon from "@mui/icons-material/Assignment";
+
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { logOut } from "../modules/userLogin";
@@ -98,7 +103,17 @@ export default function Nav() {
           <ListItem key={text} disablePadding>
             <ListItemButton>
               <ListItemIcon>
-                <InboxIcon />
+                {text === "로그아웃" ? (
+                  <LogoutIcon />
+                ) : text === "마이페이지" ? (
+                  <PermIdentityIcon />
+                ) : text === "지도" ? (
+                  <FmdGoodIcon />
+                ) : text === "로그인" ? (
+                  <LoginIcon />
+                ) : text === "회원가입" ? (
+                  <AssignmentIcon />
+                ) : null}
               </ListItemIcon>
               <ListItemText primary={text} onClick={navItemClick} />
             </ListItemButton>
