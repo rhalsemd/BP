8a4b8e6fe3b9f2eef13e3236d3308925b4824b0a
rhalsemd <rhalsemd@naver.com>
2023-02-08 09:52:39 +0900

    [BE] UPDATE : 이미지 인식 기능 multipartfile->byte로 변환, 반납 부분에 주석 추가, 테스트 완료
    Former-commit-id: f8b8222a2c8df695f3d9f0cd0da55721e5ca8c1b

--- backend/bpservice/src/main/java/kr/co/bpservice/controller/kiosk/DetectionController.java
+++ backend/bpservice/src/main/java/kr/co/bpservice/controller/kiosk/DetectionController.java
@@ -7,6 +7,7 @@ import kr.co.bpservice.util.error.UploadFileException;
 import kr.co.bpservice.yolov5.Detection;
 import kr.co.bpservice.yolov5.YoloV5;
 import lombok.RequiredArgsConstructor;
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfByte;
 import org.opencv.imgcodecs.Imgcodecs;
@@ -20,13 +21,13 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 
 @RequestMapping("/api/img")
 @RestController
 @RequiredArgsConstructor
 public class DetectionController {
-    static private final List<String> mimeTypes = Arrays.asList("image/png", "image/jpeg");
     private final YoloV5 inferenceSession;
 
     private DetectionController() throws OrtException, IOException {
@@ -38,10 +39,28 @@ public class DetectionController {
 
     @PostMapping(value = "/detection")
     public List<Detection> detection(@RequestBody MultipartFile uploadFile) throws OrtException, IOException {
-        if (!mimeTypes.contains(uploadFile.getContentType())) throw new UploadFileException(ErrorCode.INVALID_MIME_TYPE);
         byte[] bytes = uploadFile.getBytes();
+
         Mat img = Imgcodecs.imdecode(new MatOfByte(bytes), Imgcodecs.IMREAD_COLOR);
         List<Detection> result = inferenceSession.run(img);
         return result;
+    }
+
+    @PostMapping(value = "/detection-test")
+    public String detectionTest(@RequestBody Map<String, String> requestMap) throws OrtException, IOException {
+        byte[] bytes = Base64.decodeBase64(requestMap.get("imgData")
+                .replaceAll("data:image/png;base64,", "")
+                .replaceAll("data:image/jpeg;base64,", "")
+        );
+
+        Mat img = Imgcodecs.imdecode(new MatOfByte(bytes), Imgcodecs.IMREAD_COLOR);
+        List<Detection> result = inferenceSession.run(img);
+        String requestData = "우산이 인식되지 않았습니다.";
+        for(int i = 0; i<result.size();i++){
+            if(result.get(i).label().equals("umbrella")&&result.get(i).confidence()>=0.5f) {
+                requestData="우산이 인식되었습니다.";
+            }
+        }
+        return requestData;
     }
 }
