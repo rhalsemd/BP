6a67af3cc65e0a2584e2791a7a0d16b9a2dc3056
tmdwns950428 <tmdwns950428@naver.com>
2023-02-13 14:01:31 +0900

    [FE] UPDATE: #S08P12D104-422 키오스크 지점관리화면, 큐알 애니메이션 수정(빨간색), 대여 및 반납 0개일때 모달창 설정하기
    Former-commit-id: f562e130b98fcf19dcea97119a855e12388af858

--- frontend/kiosk/src/kiosk/routes/Home.js
+++ frontend/kiosk/src/kiosk/routes/Home.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import KioskBranchLocation from '../components/button/KioskBranchLocation'
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react'
+import axios from "axios";
 
 const HomeDiv = css`
   display: flex;
@@ -22,29 +23,22 @@ const HomeSemiDiv = css`
 `
 
 function Home() {
-  const items = [
-    { id: 1, name: '구미SSAFY점' },
-    { id: 2, name: '구미역점' },
-    { id: 3, name: '대구역점' },
-    { id: 4, name: '구미강동 메가박스점' },
-    { id: 5, name: '타이어뱅크 구미인동점' },
-    { id: 6, name: '하삼동커피 황상점' },
-    { id: 7, name: '구미전자공업고등학교점' },
-    { id: 8, name: '고용노동부 구미지청점' },
-    { id: 9, name: '구미 진미동행정복지센터점' },
-    { id: 10, name: '구미공단우체국점' },
-    { id: 11, name: '구미 인동동행정복지센터점' },
-    { id: 12, name: '대구지방법원 구미시법원점' },
-    { id: 13, name: '구미 금오랜드점' },
-    { id: 14, name: '금오산점' },
-    { id: 15, name: '이마트 동구미점' },
-    { id: 16, name: '금오공대점' },
-    { id: 17, name: '구미대학교점' },
-    { id: 18, name: '구미종합터미널점' },
-    { id: 19, name: '롯데시네마 구미공단점' },
-    { id: 20, name: '동대구역점' },
-    { id: 21, name: '서대구역점' },
-  ]
+  const [items, setItems] = useState([]);
+
+  const setBranchSet = () => {
+    axios({
+      method: 'GET',
+      url: 'http://bp.ssaverytime.kr:8080/api/kiosk/home/kiosk-list'
+    })
+      .then((res) => {
+        setItems(res.data)
+      })
+      .catch((err) => console.log(err))
+  }
+
+  useEffect(() => {
+    setBranchSet();
+  }, [])
 
   return (
     <div css={HomeDiv}>
