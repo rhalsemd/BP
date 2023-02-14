67fd7e31808655654ea54882bdc6cd8ab1b05ff1
rigizer <rigizer@gmail.com>
2023-02-14 15:07:21 +0900

    [IoT] 라즈베리파이 키오스크 FastAPI 서버의 전체열기, 전체닫기 기능추가 및 백엔드 API의 https 설정
    Former-commit-id: 08ce8c59995e2b8609c44cf71e96714290549bbc

--- iot/main.py
+++ iot/main.py
@@ -30,8 +30,94 @@ from pyvirtualdisplay import Display
 WINDOW_OPTION = True
 KIOSK_ID = 1
 
-KIOSK_HOST = 'http://localhost:85'
-# KIOSK_HOST = 'http://bp.ssaverytime.kr:85'
+SERVO_MAX_DUTY    = 12   # 서보의 최대(180도) 위치의 주기
+SERVO_MIN_DUTY    = 3    # 서보의 최소(0도) 위치의 주기
+
+OPEN_TIME_SLEEP = 0.2
+CLOSE_TIME_SLEEP = 0.2
+OPEN_TO_CLOSE_SLEEP = 9.0
+OPEN_TO_CLOSE_SLEEP2 = 30.0
+
+# KIOSK_HOST = 'http://localhost:85'
+KIOSK_HOST = 'https://bp.ssaverytime.kr:85'
+
+motor = {
+    1: 17, 
+    2: 27, 
+    3: 22, 
+    4: 23
+}
+motor_degree = {
+    1: (180, 90), 
+    2: (0, 110), 
+    3: (90, 180), 
+    4: (90, 0)
+}
+proximity_sensor = {
+    1: 21, 
+    2: 20, 
+    3: 00, 
+    4: 00
+}
+
+def setServoPos(pvm, degree):
+    # 각도는 180도를 넘을 수 없다.
+    if degree > 180:
+        degree = 180
+
+    # 각도(degree)를 duty로 변경한다.
+    duty = SERVO_MIN_DUTY + (degree * (SERVO_MAX_DUTY - SERVO_MIN_DUTY) / 180.0)
+
+    # 변경된 duty값을 서보 pwm에 적용
+    pvm.ChangeDutyCycle(duty)
+
+def open_holder(holderNum):
+    motor_pin = motor.get(holderNum)
+    p_sensor_pin = proximity_sensor.get(holderNum)
+
+    # GPIO 세팅
+    GPIO.setmode(GPIO.BCM) 
+    GPIO.setup(motor_pin, GPIO.OUT)
+    GPIO.setup(p_sensor_pin, GPIO.IN)
+
+    # holderid에 해당하는 motor_pin, p_sensor_pin에 대해 GPIO 명령어 전송
+    pvm = GPIO.PWM(motor_pin, 50)
+    pvm.start(0)
+
+    setServoPos(pvm, motor_degree.get(holderNum)[0])
+    time.sleep(OPEN_TIME_SLEEP)
+
+    # GPIO 통신 종료
+    pvm.stop()
+    GPIO.cleanup()
+
+def close_holder(holderNum):
+    motor_pin = motor.get(holderNum)
+    p_sensor_pin = proximity_sensor.get(holderNum)
+
+    # GPIO 세팅
+    GPIO.setmode(GPIO.BCM) 
+    GPIO.setup(motor_pin, GPIO.OUT)
+    GPIO.setup(p_sensor_pin, GPIO.IN)
+
+    # holderid에 해당하는 motor_pin, p_sensor_pin에 대해 GPIO 명령어 전송
+    pvm = GPIO.PWM(motor_pin, 50)
+    pvm.start(0)
+
+    # 근접센서 결과
+    # 0: 근접한 물체가 있음, 1: 근접한 물체가 없음
+    p_sensor_result = GPIO.input(p_sensor_pin)
+    brollyResult = not bool(p_sensor_result)
+
+    # 모터 0도
+    setServoPos(pvm, motor_degree.get(holderNum)[1])
+    time.sleep(CLOSE_TIME_SLEEP)
+
+    # GPIO 통신 종료
+    pvm.stop()
+    GPIO.cleanup()
+
+    return brollyResult
 
 # 크롬 드라이버 설정
 print('[INFO] 크롬 드라이버 세팅')
@@ -73,19 +159,6 @@ driver.get(url=kiosk_url)
 
 app = FastAPI()
 
-motor = {
-    1: 21, 
-    2: 20, 
-    3: 26, 
-    4: 19
-}
-proximity_sensor = {
-    1: 18, 
-    2: 17, 
-    3: 27, 
-    4: 22
-}
-
 @app.get('/')
 async def root():
     return {'message': 'root directory'}
@@ -97,34 +170,14 @@ async def open(caseId: int, holderNum: int, action: str, depositeMoney: int=0, p
     elif action == 'return':    # 반납
         driver.get(url=f'{KIOSK_HOST}/kiosk/{caseId}/return/guide/{holderNum}')
 
-    motor_pin = motor.get(holderNum)
-    p_sensor_pin = proximity_sensor.get(holderNum)
+    open_holder(holderNum)
 
-    # GPIO 세팅
-    GPIO.setmode(GPIO.BCM) 
-    GPIO.setup(motor_pin, GPIO.OUT)
-    GPIO.setup(p_sensor_pin, GPIO.IN)
-
-    # holderid에 해당하는 motor_pin, p_sensor_pin에 대해 GPIO 명령어 전송
-    pvm = GPIO.PWM(motor_pin, 50)
-    pvm.start(0.0)
-
-    pvm.ChangeDutyCycle(3.0)
-    time.sleep(10.0)
-
-    # 근접센서 결과
-    # 0: 근접한 물체가 있음, 1: 근접한 물체가 없음
-    p_sensor_result = GPIO.input(p_sensor_pin)
-    brollyResult = not bool(p_sensor_result)
-
-    pvm.ChangeDutyCycle(7.5)
-    time.sleep(1.0)                     
-    pvm.ChangeDutyCycle(0.0)
-    time.sleep(1.0)
-
-    # GPIO 통신 종료
-    pvm.stop()
-    GPIO.cleanup()
+    if action == 'rent':
+        time.sleep(OPEN_TO_CLOSE_SLEEP)
+    elif action == 'return':
+        time.sleep(OPEN_TO_CLOSE_SLEEP2)
+        
+    brollyResult = close_holder(holderNum)
 
     if action == 'rent':        # 대여
         if brollyResult == False:
@@ -143,9 +196,23 @@ async def open(caseId: int, holderNum: int, action: str, depositeMoney: int=0, p
         ,'brollyResult': brollyResult
     }
 
+@app.get('/open-all/{kioskId}')
+async def open_all(kioskId):
+    open_holder(1)
+    open_holder(2)
+    open_holder(3)
+    open_holder(4)
+
+@app.get('/close-all/{kioskId}')
+async def cloase_all(kioskId):
+    close_holder(1)
+    close_holder(2)
+    close_holder(3)
+    close_holder(4)
+
 @app.get('/current-weather')
 async def current_weather(lat: float, lng: float):
-    response = requests.get('http://bp.ssaverytime.kr:8080/api/weather/current-weather?lat={}&lng={}'.format(lat, lng))
+    response = requests.get('https://bp.ssaverytime.kr:8080/api/weather/current-weather?lat={}&lng={}'.format(lat, lng))
     if response.status_code == 200:
         return {'result': response.text}
     
